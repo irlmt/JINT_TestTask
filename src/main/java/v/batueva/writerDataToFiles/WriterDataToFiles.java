862ea5069ac5465fbabd7244f1df2fcacf17d6b3
Batueva Vlada <irelmtt@gmail.com>
2024-12-29 22:22:23 +0300

    delete unused dependencies

--- src/main/java/v/batueva/writerDataToFiles/WriterDataToFiles.java
+++ src/main/java/v/batueva/writerDataToFiles/WriterDataToFiles.java
@@ -1,22 +1,8 @@
 package v.batueva.writerDataToFiles;
 
 import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
-import lombok.Getter;
 import v.batueva.dataParser.DataParser;
-import v.batueva.statistics.statisticsFloat.StatisticsFloat;
-import v.batueva.statistics.statisticsInteger.StatisticsInteger;
-import v.batueva.statistics.statisticsString.StatisticsString;
-import v.batueva.writerDataToFiles.validators.fileNameValidator.FileNameValidator;
-import v.batueva.writerDataToFiles.validators.notCommandValidator.NotCommandValidator;
-
-import java.io.*;
-import java.math.BigDecimal;
-import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.PatternSyntaxException;
 
 public class WriterDataToFiles {
     private JCommander jCommander;
